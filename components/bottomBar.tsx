2687777a95b6486a963238f921d8be156a0666eb
WolfEYc <isaacwolfsite@gmail.com>
2022-01-19 22:18:55 +0000

    builds

--- components/bottomBar.tsx
+++ components/bottomBar.tsx
@@ -3,7 +3,6 @@ import BottomBarScreen from "./bottonBarScreen";
 
 import BottomBarMic from "./bottomBarMic";
 
-
 const BottomBar = () => {
     return(
         <div className = "fixed bottom-0 m-0 flex bg-gray-900 h-16 border-t border-gray-800 shadow-xl pt-3 justify-center w-5/6">
